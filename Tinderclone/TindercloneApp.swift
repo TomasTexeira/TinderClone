e2ec0bb828e4be9086fc8d67b68414a067b12cbd
TomasTexeira <tom.tex2322@gmail.com>
2021-06-16 15:08:16 -0300

    Initial Commit

---
+++ Tinderclone/TindercloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TindercloneApp.swift
+//  Tinderclone
+//
+//  Created by TomTex on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TindercloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
